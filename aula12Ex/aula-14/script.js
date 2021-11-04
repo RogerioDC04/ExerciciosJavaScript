0160428e9ab095f95c92ecec669eba57781abf26
RogerioDC04 <dominguescanilharogerio@gmail.com>
2021-11-04 11:27:20 -0300

    Atualizaçao

---
+++ aula12Ex/aula-14/script.js
@@ -0,0 +1,16 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagen')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora sao ${hora} horas`
+    if (hora >= 0 && hora < 12){
+       img.scr = 'Manha.png'
+    }else if (hora >= 12 && hora <18) {
+        img.scr = 'Tarde.png'
+    }else {
+        img.scr = 'Noite.png'
+    }
+}
+
